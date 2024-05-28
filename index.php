9dee93ae6e88f043d6df2b9f211ddd9320c1ed86
chems <chayma.kilic@lepoles.org>
2024-05-28 17:01:00 +0200

    myfairbook.php
    debut site book

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include __DIR__ ."/public/header.html.php"?>
+<?php include __DIR__ ."/public/nav.html.php"?>
+
+<main></main>
+<?php include __DIR__ ."/public/footer.html.php"?>
---
+++ public/header.html.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <base href="http://localhost/myfairbook/">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+<body> 
+<div id="principale">
+    <header>
+        <div class="reseaux">
+            <div class="reseau"></div>
+
+   
+    </div>
+        <div class="">
+
+   
+    </div>
+        </header>
